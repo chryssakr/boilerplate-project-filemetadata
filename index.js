e32f835d60f6f00bb4f22cf680886089a92384fc
Chryssa Karatza <chryssakaratza@gmail.com>
2024-04-26 17:35:34 +0100

    upload solution

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
@@ -11,8 +12,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: "./puplic/data/uploads" })
 
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
